2aeb5888bbf8cdfee70bd229d2e8fe0151e18a18
Johnatanrincon05 <Johnatandavidcallejas@gmail.com>
2024-06-14 01:51:43 -0500

    SomeStyleFixes[DescriptionAdded]

--- index.php
+++ index.php
@@ -16,7 +16,9 @@ if (!isset($_SESSION['secret_number'])) {
     <form action="game.php" method="post">
         <h1>Picas y Fijas</h1>
         <input type="hidden" value="<?php echo $_SESSION['secret_number']?>">
-        <label for="user">Ingresa un número de 4 dígitos:</label>
+        <label id="label1" for="user">  Picas y Fijas es un juego en el que debes adivinar un número secreto de 4 dígitos. 
+            Una "Fija" indica un dígito correcto en la posición correcta, 
+            y una "Pica" indica un dígito correcto en la posición incorrecta. <br> <br> Ingresa un número de 4 dígitos:</label>
         <input type="text" id="user" name="user" maxlength="4" required pattern="\d{4}" title="Debe contener exactamente 4 dígitos">
         <button type="submit">! Intenta adivinar ¡</button>
         <?php
